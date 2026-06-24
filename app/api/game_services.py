7407ff2bb182651362684fc046d5677ffcb29834
athanase25537 <andriamasyathanase.nazy@gmail.com>
2026-06-24 16:27:21 +0300

    update: model to game

--- app/api/game_services.py
+++ app/api/game_services.py
@@ -24,8 +24,9 @@ class GameService:
         game.turn = model.turn
         game.winner = model.winner
         game.status = model.status
-        game.pieces_left = model.pieces_left
-        game.players = model.players
+        # Convertir les clés des JSON en int
+        game.pieces_left = {int(k): v for k, v in model.pieces_left.items()} if model.pieces_left else {}
+        game.players = {int(k): v for k, v in model.players.items()} if model.players else {}
         return game
 
     def _game_to_model(self, game: GameState, model: Optional[GameModel] = None) -> GameModel:
